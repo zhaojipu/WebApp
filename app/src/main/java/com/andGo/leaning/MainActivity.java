acbde560393af047234ffa6e77fed04c7ce2802d
zhaojipu <zhaojipu@99114.com>
2017-03-21 17:57:25 +0800

    使用联通商城的链接，方便浏览

--- app/src/main/java/com/andGo/leaning/MainActivity.java
+++ app/src/main/java/com/andGo/leaning/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -14,11 +15,15 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     WebView mWebview;
     WebSettings mWebSettings;
-    TextView beginLoading,endLoading,loading,mtitle;
+    TextView beginLoading, endLoading, loading, mtitle;
+    //使用的是联通商城的链接
+    String url = "http://m.10010.com/bj/?first=true&version=android@5.01&ticket=xkzc9kty5d989672d6c1c7364257c3ee9bd77496ivdv6pna";
 
+    //    String url="http://m.10010.com/bj";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
 
@@ -30,7 +35,15 @@ public class MainActivity extends AppCompatActivity {
 
         mWebSettings = mWebview.getSettings();
 
-        mWebview.loadUrl("http://www.baidu.com/");
+//设置自适应屏幕，两者合用
+        mWebSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        mWebSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+
+        //缩放操作
+        mWebSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        mWebSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        mWebSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        mWebview.loadUrl(url);
 
 
         //设置不用系统浏览器打开,直接显示在当前Webview
